f7f67e3bdafe186522a3650a5f39ae553af7f544
Maxime Reynier <maxime.rnrn@gmail.com>
2017-03-13 12:18:15 +0100

    supprimé :        ComputerDataBase/src/com/excilys/persistence/ComputerDAO.java modifié :         ComputerDataBase/src/com/excilys/test/java/ComputerDAOTest.java

---
+++ ComputerDataBase/src/com/excilys/persistence/java/ComputerDAO.java
@@ -0,0 +1,54 @@
+package com.excilys.persistence.java;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.excilys.connection.java.ConnectionDB;
+import com.excilys.model.java.Computer;
+
+public class ComputerDAO {
+	  private Connection conn;
+		
+	  public ComputerDAO() {
+	    
+	    this.conn = ConnectionDB.getInstance().getConn();
+	    
+	  }
+	
+	  public boolean create(Computer cp) {
+	    return false;
+	  }
+
+	  public boolean delete(Computer cp) {
+	    return false;
+	  }
+	   
+	  public boolean update(Computer cp) {
+	    return false;
+	  }
+	   
+	  public Computer find(int id) {
+	    Computer computer = new Computer();      
+	      
+	    try {
+	      String sql = "SELECT c.id, c.name, c.introduced, c.discontinued, e.name FROM computer c Left join company e ON c.company_id=e.id WHERE c.id='"+id+"';";
+	      System.out.println(sql);
+	      ResultSet result = this.conn.createStatement(
+	        ResultSet.TYPE_SCROLL_INSENSITIVE,
+	        ResultSet.CONCUR_READ_ONLY).executeQuery(sql);
+	        
+	      if(result.first())
+	        computer = new Computer(
+	          result.getInt("c.id"),
+	          result.getString("c.name"),
+	          result.getDate("c.introduced").toLocalDate(),
+	          result.getString("e.name")
+	          );         
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+	    System.out.println(computer.toString());
+	    return computer;
+	  }
+	}
