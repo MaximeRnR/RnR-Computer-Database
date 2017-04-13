9489db6432b7c33e657cd56c10f5dd100177931d
Maxime Reynier <maxime.rnrn@gmail.com>
2017-04-13 16:28:32 +0200

    add test user

--- Docker/mysql/db/2-PRIVILEGES.sql
+++ Docker/mysql/db/2-PRIVILEGES.sql
@@ -8,3 +8,13 @@
 
   FLUSH PRIVILEGES;
 
+  #-----------------------------------
+  #USER RIGHTS MANAGEMENT
+  #-----------------------------------
+  CREATE USER 'admincdb'@'172.20.0.%' IDENTIFIED BY 'qwerty1234';
+
+  GRANT ALL PRIVILEGES ON `computer-database-db-test`.* TO 'admincdb'@'172.20.0.%' WITH GRANT OPTION;
+
+
+  FLUSH PRIVILEGES;
+
