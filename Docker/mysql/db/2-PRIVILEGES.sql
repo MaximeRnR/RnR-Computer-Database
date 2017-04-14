3ec0d99e01eea8e0e39cab4953b741d157a00da1
Maxime Reynier <maxime.rnrn@gmail.com>
2017-04-14 15:09:40 +0200

    fix for docker cloud

--- Docker/mysql/db/2-PRIVILEGES.sql
+++ Docker/mysql/db/2-PRIVILEGES.sql
@@ -1,9 +1,9 @@
   #-----------------------------------
   #USER RIGHTS MANAGEMENT
   #-----------------------------------
-  CREATE USER 'admincdb'@'172.18.0.%' IDENTIFIED BY 'qwerty1234';
+  CREATE USER 'admincdb'@'172.21.0.%' IDENTIFIED BY 'qwerty1234';
 
-  GRANT ALL PRIVILEGES ON `computer-database-db-test`.* TO 'admincdb'@'172.18.0.%' WITH GRANT OPTION;
+  GRANT ALL PRIVILEGES ON `computer-database-db-test`.* TO 'admincdb'@'172.21.0.%' WITH GRANT OPTION;
 
 
   FLUSH PRIVILEGES;
